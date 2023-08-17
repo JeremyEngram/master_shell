ab4a089ad16c42e9815e7419c944dd263ddcf91d
JeremyEngram <65774401+JeremyEngram@users.noreply.github.com>
2023-08-16 23:30:11 -0400

    NULL

--- recursivearm.sh
+++ recursivearm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find all files in the home directory that have a .sh extension
-files=$(find /home/siebel -maxdepth 1 -name "*.sh")
+files=$(find . -maxdepth 1 -name "*.sh")
 
 # Loop through each file and make it executable
 for file in $files; do
@@ -9,7 +9,7 @@ for file in $files; do
 done
 
 # Find all files in the directory that have a .sh extension
-files=$(find /path/to/directory -maxdepth 1 -name "*.sh")
+files=$(find . -maxdepth 1 -name "*.sh")
 
 # Loop through each file and check if it has a shebang line
 for file in $files; do
@@ -19,7 +19,7 @@ for file in $files; do
 done
 
 # Find all files in the directory that have a shebang line
-files=$(grep -rl "^#!/bin/bash" /path/to/directory)
+files=$(grep -rl "^#!/bin/bash" .)
 
 # Loop through each file and rename it if it doesn't already have a .sh extension
 for file in $files; do
